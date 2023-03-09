cc84a67a19add1e9045dd050f4aafb82a9b11304
ngoc <hoangngoc.10819@gmail.com>
2023-03-09 16:30:39 +0700

    chỉnh sửa bên master

--- myapp/app.js
+++ myapp/app.js
@@ -24,9 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+333333333333333333333333333333333333333333333333333
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+4444444444444444444444444444444444444444444444444
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
